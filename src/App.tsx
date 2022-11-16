5a3cb4cedff712e73583250c4dd2c1fb7d7dd37e
dmitry <ggbox@mail.ru>
2022-11-16 19:10:02 +0400

    eyes hardcode

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import Eye from './components/eye/Eye'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='eye-wrapper'>
+        <Eye/>
+        <Eye/>
+      </div>
+      <div className='mouth-wrapper'>
+        <h2>HELLO WORLD</h2>
+      </div>
+    </div>
+
+      
+  )
+}
+
+export default App
---
+++ src/components/eye/Eye.tsx
@@ -0,0 +1,16 @@
+import './eye.css'
+
+function Eye() {
+
+  return (
+    <div className="eye">
+        <div className='iris'>
+            <div className='pupil'></div>
+
+        </div>
+        <div className='highlight'></div>
+    </div>
+  )
+}
+
+export default Eye
